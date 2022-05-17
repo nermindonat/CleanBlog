c90634d6f8ec0e09d37fe71a0e18213d2ccd7467
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-05-17 16:24:34 +0300

    Public views klasörleri ve .ejs uzantılı dosyalar oluşturuldu.

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
 
 const app = express();
 
+// Template Engine 
+app.set("view engine", "ejs")
+
+
+// Middleware
+app.use(express.static('public'))
+
+
+// Routers
 app.get('/', (req, res) => {
-
-    const blog = {
-        id: 1,
-        title: "Blog title", 
-        description: "Blog description"
-    }
-
-    res.send(blog)
+    res.render('index')
 })
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+
 
 const port = 3000;
 
